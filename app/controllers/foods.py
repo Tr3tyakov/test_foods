9eb05a04ab590b2cef8494312b23fc85f023764d
tretyakov <Imtretyakov@rambler.ru>
2024-08-08 10:08:02 +0300

    Изменено наименование

--- app/controllers/foods.py
+++ app/controllers/foods.py
@@ -18,37 +18,37 @@ router = APIRouter(tags=["food"], prefix="/food")
     response_model=List[ResponseCategorySchema],
 )
 async def get_foods(
-    request: Request,
-    service: FoodService = Depends(),
-    is_vegan: Optional[bool] = Query(False, description="Веганское блюдо"),
-    is_special: Optional[bool] = Query(False, description="Особенность блюда"),
+        request: Request,
+        service: FoodService = Depends(),
+        is_vegan: Optional[bool] = Query(False, description="Веганское блюдо"),
+        is_special: Optional[bool] = Query(False, description="Особенность блюда"),
 ):
     return await service.get_foods(request, is_vegan=is_vegan, is_special=is_special)
 
 
 @router.get("/{id}", description="Получение конкретного блюда")
 async def get_food(
-    request: Request, id: int, service: FoodService = Depends()
+        request: Request, id: int, service: FoodService = Depends()
 ) -> ResponseFoodSchema:
     return await service.get_food(request, id=id)
 
 
 @router.post("/", description="Создание блюда")
 async def create_food(
-    request: Request, data: CreateFoodSchema, service: FoodService = Depends()
+        request: Request, data: CreateFoodSchema, service: FoodService = Depends()
 ):
     return await service.create_food(request, data=data)
 
 
 @router.put("/{id}", description="Обновление блюда")
 async def update_food(
-    request: Request, data: UpdateFoodSchema, id: int, service: FoodService = Depends()
+        request: Request, data: UpdateFoodSchema, id: int, service: FoodService = Depends()
 ) -> ResponseFoodSchema:
     return await service.update_food(request, data, id=id)
 
 
 @router.patch("/{id}", description="Обновление блюда")
-async def get_food(
-    request: Request, id: int, data: PatchFoodSchema, service: FoodService = Depends()
+async def patch_food(
+        request: Request, id: int, data: PatchFoodSchema, service: FoodService = Depends()
 ) -> ResponseFoodSchema:
     return await service.patch_food(request, data, id=id)
